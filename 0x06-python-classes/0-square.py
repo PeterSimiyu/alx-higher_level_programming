c77dbdf76f166eeec2a41c864585bbbc5b8ef4d8
PeterSimiyu <simiyupeter10@gmail.com>
2021-12-07 12:36:58 -0800

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Module 0-square
-Defines a square
+Module 0-sqaure
+Defines a sqaure
 """
 
+
 class Square:
-    """An empty square template"""
+    """ An empty square template """
     pass
